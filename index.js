123acc202f7951f1af1e84d8c0f1fa32ff000bc0
Adam <adam@mac.attlocal.net>
2019-09-26 20:37:15 -0500

    Add redux store and action reducer

--- index.js
+++ index.js
@@ -4,52 +4,75 @@
 /*global window*/
 /*jshint esversion:6*/
 window.onload = () => {
-  function counter(state, action) {
-        if (typeof state === 'undefined') {
-          return 0;
-        }
 
-        switch (action.type) {
-          case 'INCREMENT':
-            return state + 1;
-          case 'DECREMENT':
-            return state - 1;
-          default:
-            return state;
-        }
-      }
-
-      var store = Redux.createStore(counter);
-      var valueEl = document.getElementById('value');
-
-      function render() {
-        valueEl.innerHTML = store.getState().toString();
-      }
-
-      render();
-      store.subscribe(render);
-
-      document.getElementById('increment')
-        .addEventListener('click', function () {
-          store.dispatch({ type: 'INCREMENT' });
+  function buttonPressed(state, action) {
+    if (typeof state === 'undefined') {
+      return 0;
+    }
+    switch (action.type) {
+      case 'ADD':
+        return Object.assign({}, state, {
+          keysPressed: [...state.keysPressed, action.keyPressed]
         });
-
-      document.getElementById('decrement')
-        .addEventListener('click', function () {
-          store.dispatch({ type: 'DECREMENT' });
-        });
-
-      document.getElementById('incrementIfOdd')
-        .addEventListener('click', function () {
-          if (store.getState() % 2 !== 0) {
-            store.dispatch({ type: 'INCREMENT' });
-          }
-        });
-
-      document.getElementById('incrementAsync')
-        .addEventListener('click', function () {
-          setTimeout(function () {
-            store.dispatch({ type: 'INCREMENT' });
-          }, 1000);
-        });
+      default:
+        return state;
+    }
+  }
 };
+
+function addKey(key){
+  return {
+    type: 'ADD',
+    keyPressed: key
+  };
+}
+/*
+function counter(state, action) {
+      if (typeof state === 'undefined') {
+        return 0;
+      }
+
+      switch (action.type) {
+        case 'INCREMENT':
+          return state + 1;
+        case 'DECREMENT':
+          return state - 1;
+        default:
+          return state;
+      }
+    }
+
+    var store = Redux.createStore(counter);
+    var valueEl = document.getElementById('value');
+
+    function render() {
+      valueEl.innerHTML = store.getState().toString();
+    }
+
+    render();
+    store.subscribe(render);
+
+    document.getElementById('increment')
+      .addEventListener('click', function () {
+        store.dispatch({ type: 'INCREMENT' });
+      });
+
+    document.getElementById('decrement')
+      .addEventListener('click', function () {
+        store.dispatch({ type: 'DECREMENT' });
+      });
+
+    document.getElementById('incrementIfOdd')
+      .addEventListener('click', function () {
+        if (store.getState() % 2 !== 0) {
+          store.dispatch({ type: 'INCREMENT' });
+        }
+      });
+
+    document.getElementById('incrementAsync')
+      .addEventListener('click', function () {
+        setTimeout(function () {
+          store.dispatch({ type: 'INCREMENT' });
+        }, 1000);
+      });
+      */
