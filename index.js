d523695883af1175e9afca5ace98c74d5a4994d9
Adam <adam@mac.attlocal.net>
2019-09-29 12:55:00 -0500

    Fix zero key bug

--- index.js
+++ index.js
@@ -6,22 +6,20 @@
 /*global console*/
 
 window.onload = () => {
-
   Array.from(document.querySelectorAll('.numeral')).forEach(function (elem) {
     elem.addEventListener("click", function () {
       store.dispatch(addKey(this.innerText));
     });
   });
-  
-  document.querySelector(".erase").addEventListener("click",function(){
+  document.querySelector(".erase").addEventListener("click", function () {
     store.dispatch(erase(this.innerText));
   });
-  
-  document.querySelector(".bspace").addEventListener("click",function(){
+  document.querySelector(".bspace").addEventListener("click", function () {
     store.dispatch(bspace(this.innerText));
   });
-                                                    
-                                                    
+  document.querySelector(".zero").addEventListener("click", function () {
+    store.dispatch(addZero(this.innerText));
+  });
 
   function buttonPressed(state, action) {
     if (typeof state === 'undefined') {
@@ -35,22 +33,27 @@ window.onload = () => {
           topDisplay: [...state.keysPressed, action.keyPressed].join(''),
           bottomDisplay: [...state.keysPressed, action.keyPressed].join('')
         });
+      case 'ADDZERO':
+        return state.topDisplay === '0' ? state : Object.assign({}, state, {
+          keyPressed: action.keyPressed,
+          keysPressed: [...state.keysPressed, action.keyPressed],
+          topDisplay: [...state.keysPressed, action.keyPressed].join(''),
+          bottomDisplay: [...state.keysPressed, action.keyPressed].join('')
+        });
       case 'ERASE':
         return Object.assign({}, state, {
           keyPressed: action.keyPressed,
-          keysPressed: [...state.keysPressed, action.keyPressed],
+          keysPressed: [],
           topDisplay: '0',
           bottomDisplay: '0'
         });
       case 'BSPACE':
         return Object.assign({}, state, {
           keyPressed: action.keyPressed,
-          keysPressed: [...state.keysPressed, action.keyPressed],
-          topDisplay: state.topDisplay.slice(0,-1),
-          bottomDisplay: state.bottomDisplay.slice(0,-1)
+          keysPressed: [...state.keysPressed.slice(0, -1)],
+          topDisplay: state.topDisplay.slice(0, -1),
+          bottomDisplay: state.bottomDisplay.slice(0, -1)
         });
-          
-     
       default:
         return state;
     }
@@ -63,16 +66,23 @@ window.onload = () => {
     };
   }
 
-  function erase(key){
+  function erase(key) {
     return {
       type: 'ERASE',
       keyPressed: key
     };
   }
-  
-  function bspace(key){
+
+  function bspace(key) {
     return {
       type: 'BSPACE',
+      keyPressed: key
+    };
+  }
+
+  function addZero(key) {
+    return {
+      type: 'ADDZERO',
       keyPressed: key
     };
   }
@@ -81,14 +91,12 @@ window.onload = () => {
     document.getElementById("topNum").innerHTML = store.getState().topDisplay;
     document.getElementById("bottomNum").innerHTML = store.getState().bottomDisplay;
   }
-
   let initialState = {
-    keyPressed: '0',
+    keyPressed: '',
     keysPressed: [],
     topDisplay: '0',
     bottomDisplay: '0'
   };
-
   let store = Redux.createStore(buttonPressed, initialState);
   store.subscribe(render);
 
